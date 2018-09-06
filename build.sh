3b7d0eebe20a6262dd066df49a55918833a7624a
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-09-06 14:38:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@
 # SOFTWARE.
 
 platforms=("darwin/386" "darwin/amd64" "dragonfly/amd64" "freebsd/386" "freebsd/amd64" "freebsd/arm" "linux/386" "linux/amd64" "linux/arm" "linux/arm64" "linux/ppc64" "linux/ppc64le" "linux/mips" "linux/mipsle" "linux/mips64" "linux/mips64le" "netbsd/386" "netbsd/amd64" "netbsd/arm" "openbsd/386" "openbsd/amd64" "openbsd/arm" "plan9/386" "plan9/amd64" "solaris/amd64" "windows/386" "windows/amd64")
-rm -rf bin
+rm -rf bin deb
 mkdir bin
 
 version=2.0
